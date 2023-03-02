881f17b08a909f0ec17e9baf78611c10a8725e8c
yamuna93 <64839904+yamuna93@users.noreply.github.com>
2023-03-02 12:25:52 -0500

    created html and linked it to .ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: 'main.html',
+  templateUrl: 'main.html',
 })
 export class App {
   name = 'Angular';
