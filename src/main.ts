925ceac83b833fccc7f865901c7ee8bc1cc7e926
yamuna93 <64839904+yamuna93@users.noreply.github.com>
2023-03-02 13:24:52 -0500

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,35 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+import { Cat } from './cat-model';
+import { CatService } from './cat';
+
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    HttpClientModule, MatCardModule, InfiniteScrollModule],
   templateUrl: 'main.html',
 })
 export class App {
   name = 'Angular';
+  page = 1;
+cats: Cat[] = [];
+constructor(private catService: CatService) {}
+ngOnInit(): void {
+  this.catService.getCats(this.page).subscribe((cats: Cat[]) => {
+        this.cats = cats;
+     });
+ }
+ onScroll(): void {
+  this.catService.getCats(++this.page).subscribe((cats: Cat[]) => {
+      this.cats.push(...cats);
+    });
+}
 }
 
 bootstrapApplication(App);
