20ac6a5937ffa186a22ca13287a23e4a7300f500
yamuna93 <64839904+yamuna93@users.noreply.github.com>
2023-03-02 12:24:40 -0500

    1 file modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
+  template: 'main.html',
 })
 export class App {
   name = 'Angular';
